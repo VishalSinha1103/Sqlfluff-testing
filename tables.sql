65a6380117e8e2f4a37ee90d8c951f25a9649c54
VishalSinha1103 <111853779+VishalSinha1103@users.noreply.github.com>
2025-03-26 16:37:19 +0530

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,8 +1,8 @@
 -- Create users table
 CREATE TABLE users (
     id INT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100),
+    "name" VARCHAR(100),
+    "email" VARCHAR(100),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
