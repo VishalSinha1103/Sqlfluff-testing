22f016e5e3f568cb271092f0751e1128173b2354
VishalSinha1103 <111853779+VishalSinha1103@users.noreply.github.com>
2025-04-14 10:27:41 +0530

    Delete Sql-fluff directory

--- Sql-fluff/sqlfluff_plugin_iceberg/__init__.py
+++
@@ -1,1 +0,0 @@
-# SQLFluff Iceberg Plugin Init
--- Sql-fluff/sqlfluff_plugin_iceberg/plugin.py
+++
@@ -1,9 +0,0 @@
-from sqlfluff.core.plugin import hookimpl
-from sqlfluff.dialects.dialect_snowflake import dialect as snowflake_dialect
-from .iceberg_grammar import CreateIcebergTableSegment
-
-@hookimpl
-def get_dialect_patch():
-    dialect = snowflake_dialect.copy_as("snowflake_with_iceberg")
-    dialect.replace(CreateIcebergTableSegment)
-    return dialect
