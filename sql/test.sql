2609609459b48a524dbb8e1faaa3adf3fc9cd3c5
VishalSinha1103 <111853779+VishalSinha1103@users.noreply.github.com>
2025-04-20 20:12:08 +0530

    Create test.sql

---
+++ sql/test.sql
@@ -0,0 +1,9 @@
+-- Initial table creation
+CREATE OR REPLACE TABLE iceberg_table (
+    id INT,
+    name STRING
+)
+USING ICEBERG;
+
+-- Add new column in schema evolution
+ALTER TABLE iceberg_table ADD COLUMN created_at TIMESTAMP_LTZ;
