4920e4ffccd9ed39b8f0fc16456d29b885d0fc58
VishalSinha1103 <111853779+VishalSinha1103@users.noreply.github.com>
2025-04-22 19:37:20 +0530

    Delete sqlfluff-plugin-iceberg directory

--- sqlfluff-plugin-iceberg/sqlfluff_plugin_iceberg/plugin.py
+++
@@ -1,7 +0,0 @@
-from sqlfluff.dialects import dialect_snowflake
-from sqlfluff.core.dialects import Dialect
-
-def load_dialect() -> Dialect:
-    dialect = dialect_snowflake.Dialect.copy_as("snowflake_with_iceberg")
-    # (register grammar additions here...)
-    return dialect
