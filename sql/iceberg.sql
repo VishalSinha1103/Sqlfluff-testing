04d6a091dd4bc31ad26bfe05170ff2b4b0740157
VishalSinha1103 <111853779+VishalSinha1103@users.noreply.github.com>
2025-04-22 19:14:18 +0530

    Update iceberg.sql

--- sql/iceberg.sql
+++ sql/iceberg.sql
@@ -1,4 +1,4 @@
-CREATE ICEBERG TABLE IF NOT EXISTS users (
+CREATE ICEBERG TABLE IF NOT EXISTS testin (
     id INT PRIMARY KEY,
     "name" VARCHAR(100),         -- Escaped to avoid reserved keyword issue
     "email" VARCHAR(100),        -- Escaped to avoid reserved keyword issue
