5d5b87bd250f2133d6694be9eb4f76c275b9e39e
VishalSinha1103 <111853779+VishalSinha1103@users.noreply.github.com>
2025-04-24 16:52:50 +0530

    Update iceberg.sql

--- sql/iceberg.sql
+++ sql/iceberg.sql
@@ -1,6 +1,9 @@
-CREATE ICEBERG TABLE IF NOT EXISTS testin (
-    id INT PRIMARY KEY,
-    "name" VARCHAR(100),         -- Escaped to avoid reserved keyword issue
-    "email" VARCHAR(100),        -- Escaped to avoid reserved keyword issue
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+CREATE TABLE IF NOT EXISTS ULU_DB_SIT.IIPAS.TABLE5 (
+    INSUP_FLAG STRING COMMENT 'INSERT and UPDATE Flag',
+    SETID STRING COMMENT '',
+    MODIFIED_TIMESTAMP TIMESTAMP
+)
+COMMENT = ''
+CATALOG = 'SNOWFLAKE'
+EXTERNAL_VOLUME = 'ULUDM_DEV_EXT_VOL_SILVER_ICEBERG_EXVOL'
+BASE_LOCATION = 'oeds/ar_action_lst';
